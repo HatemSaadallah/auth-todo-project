76a6d91ee61786e3064e2c73b773907b2fc94c39
Hatem Saadallah <saadallah.r.hatem@gmail.com>
2022-04-14 12:55:44 +0300

    Deleted getting user by cache

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
 import { NestFactory, Reflector } from '@nestjs/core';
-import { CACHE_MANAGER } from '@nestjs/common';
-
 import { AppModule } from './app.module';
 import { RolesGuard } from './common/guards/roles.guard';
 import { AuthGuard } from './common/guards/auth.guard';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { CustomLogger } from './common/logger/winston.logger';
+import { UserService } from './modules/user/user.service';
+import { ConfigService } from '@nestjs/config';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: new CustomLogger
   });
-  const cacheManager = app.get(CACHE_MANAGER);
+  const userService = app.get(UserService);
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
-  app.useGlobalGuards(new AuthGuard(new Reflector(), cacheManager), new RolesGuard(new Reflector(), cacheManager));
+  app.useGlobalGuards(new AuthGuard(userService, new Reflector(), app.get(ConfigService)), new RolesGuard(new Reflector()));
   await app.listen(3000);
 }
 
